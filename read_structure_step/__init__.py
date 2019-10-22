ddc65a5f2f54806f32b33406d14304a259a3c285
Eliseo Marin-Rimoldi <emarinri@nd.edu>
2019-10-22 17:22:51 -0400

    Expose read function better

--- read_structure_step/__init__.py
+++ read_structure_step/__init__.py
@@ -12,7 +12,7 @@ from read_structure_step.read_structure import ReadStructure  # noqa: F401
 from read_structure_step.read_structure_parameters import ReadStructureParameters  # noqa: F401
 from read_structure_step.read_structure_step import ReadStructureStep  # noqa: F401
 from read_structure_step.tk_read_structure import TkReadStructure  # noqa: F401
-from . import read
+from .read import read
 
 # Handle versioneer
 from ._version import get_versions
