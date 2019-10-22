4db4448ef2ea092bb6aeb886bce2b8cd420eea48
Eliseo Marin-Rimoldi <emarinri@nd.edu>
2019-10-22 17:21:43 -0400

    Pass file name string instead of data

--- read_structure_step/formats/mol2/__init__.py
+++ read_structure_step/formats/mol2/__init__.py
@@ -4,9 +4,13 @@ from read_structure_step.formats.registries import register_format_checker
 keywords = ['@<TRIPOS>']
 
 @register_format_checker('.mol2')
-def check_format(data):
+def check_format(file_name):
 
-    if all(keyword in data for keyword in keywords):
-        return True
-    else:
-        return False
+    with open(file_name, "r") as file:
+
+        data = file.read()
+    
+        if all(keyword in data for keyword in keywords):
+            return True
+        else:
+            return False
--- read_structure_step/utils.py
+++ read_structure_step/utils.py
@@ -8,13 +8,12 @@ def guess_extension(file_name, with_file_name=False):
         (root, ext) = os.path.splitext(file_name)
         return ext.lower()
 
-    with open(file_name, 'r') as file:
-        data = file.read()
 
-        available_extensions = formats.registries.REGISTERED_FORMAT_CHECKERS.keys()
+    available_extensions = formats.registries.REGISTERED_FORMAT_CHECKERS.keys()
 
-        for extension in available_extensions:
+    for extension in available_extensions:
 
-            extension_checker = formats.registries.REGISTERED_FORMAT_CHECKERS[extension]
-            if extension_checker(data) is True:
-                return (extension)
+        extension_checker = formats.registries.REGISTERED_FORMAT_CHECKERS[extension]
+
+        if extension_checker(file_name) is True:
+            return (extension)
