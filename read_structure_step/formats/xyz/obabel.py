ab3c0d86eed6304e3afdf1fc88d7df6a17179c9d
Eliseo Marin-Rimoldi <emarinri@nd.edu>
2019-10-23 16:02:22 -0400

    Create which submodule

--- read_structure_step/formats/xyz/obabel.py
+++ read_structure_step/formats/xyz/obabel.py
@@ -3,12 +3,13 @@ import seamm
 import seamm_util
 from read_structure_step.errors import XYZError
 from read_structure_step.formats.registries import register_reader
+from ..which import which
 
 obabel_error_identifiers= ['0 molecules converted']
 
 @register_reader('.xyz')
 def load_mol2(file_name):
-        obabel_exe = _which('obabel') 
+        obabel_exe = which('obabel') 
         local = seamm.ExecLocal()
 
         result = local.run(
@@ -23,20 +24,3 @@ def load_mol2(file_name):
         structure = seamm_util.molfile.to_seamm(mol)
 
         return structure
-
-def _which(program):
-    def is_exe(fpath):
-        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
-
-    fpath, fname = os.path.split(program)
-
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            path = path.strip('"')
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
-    return None
