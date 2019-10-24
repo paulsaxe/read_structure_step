78925ded3837bd3b851ab4ab9a9dc544283f027d
Eliseo Marin-Rimoldi <emarinri@nd.edu>
2019-10-24 12:59:01 -0400

    Start tests for read function

--- tests/test_read_structure_step.py
+++ tests/test_read_structure_step.py
@@ -7,18 +7,35 @@ import pytest  # noqa: F401
 import read_structure_step  # noqa: F401
 from . import build_filenames
 
-def test_single_mol2():
 
-    mol2_file = build_filenames.build_data_filename('test.mol2')
-    mol2_parsed = read_structure_step.read(mol2_file)
+@pytest.mark.parametrize('file_name', [1, [], {}, 1.0])
+def test_read_filename_type(file_name):
 
-def test_single_xyz():
+    with pytest.raises(TypeError):
+        read_structure_step.read(file_name)
 
-    xyz_file = build_filenames.build_data_filename('spc.xyz')
-    xyz_parsed = read_structure_step.read(xyz_file)
+def test_empty_filename():
+    
+    with pytest.raises(NameError):
+        read_structure_step.read('')
 
+def test_unregistered_readers():
+    
+    with pytest.raises(KeyError):
 
-def test_single_pdb():
+        xyz_file = build_filenames.build_data_filename('spc.xyz')
+        read_structure_step.read(xyz_file, extension='.mp3')
 
-    pdb_file = build_filenames.build_data_filename('hydrolase.pdb')
-    pdb_parsed = read_structure_step.read(pdb_file)
+@pytest.mark.parametrize("extension", [None, ".xyz", "xyz", "XYZ"])
+def test_single_xyz_file(extension):
+
+    xyz_file = build_filenames.build_data_filename("spc.xyz") 
+    parsed_xyz = read_structure_step.read(xyz_file, extension=extension) 
+    
+    assert len(parsed_xyz["atoms"]["elements"]) == 3
+    assert all(atom in ["O", "H", "H"] for atom in parsed_xyz["atoms"]["elements"])
+    assert len(parsed_xyz["atoms"]["coordinates"]) == 3
+    assert all(len(point) == 3 for point in parsed_xyz["atoms"]["coordinates"])
+    assert len(parsed_xyz["bonds"]) == 2
+    assert any(bond == (2, 1, 'single') for bond in parsed_xyz["bonds"])
+    assert any(bond == (3, 1, 'single') for bond in parsed_xyz["bonds"])
