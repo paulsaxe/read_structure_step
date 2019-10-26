6aa5acc48f471ecad4ffe911760fa5a9be20c868
Eliseo Marin <meliseo@vt.edu>
2019-10-26 18:42:42 -0400

    Modify RTD theme

--- read_structure_step/errors.py
+++ read_structure_step/errors.py
@@ -1,11 +1,19 @@
+"""
+Errors for the various modules in the plugin
+"""
+
 class Error(Exception):
+""" Base class for exceptions raised by the read_structure_step"""
     pass
 
 class Mol2Error(Error):
+""" Class for exceptions raised by the Mol2 module in read_structure_step"""
     pass
 
 class XYZError(Error):
+""" Class for exceptions raised by the XYZ module in read_structure_step"""
     pass
 
 class PDBError(Error):
+""" Class for exceptions raised by the PDB module in read_structure_step"""
     pass
