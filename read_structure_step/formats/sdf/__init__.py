deebabaefec8bbe36bd30376a14c86a72ff09d2d
Eliseo Marin <meliseo@vt.edu>
2019-10-26 21:54:14 -0400

    Add SDF format

---
+++ read_structure_step/formats/sdf/__init__.py
@@ -0,0 +1,16 @@
+from . import obabel
+from read_structure_step.formats.registries import register_format_checker
+
+keywords = ['V2000', 'V3000'] 
+
+@register_format_checker('.sdf')
+def check_format(file_name):
+
+    with open(file_name, "r") as f:
+
+        data = f.read()
+    
+        if any(keyword in data for keyword in keywords):
+            return True
+        else:
+            return False
