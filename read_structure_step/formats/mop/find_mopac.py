f55db1f19a12b354ff70a643035e20ea53dcfbdd
Eliseo Marin <meliseo@vt.edu>
2019-11-24 16:46:48 -0500

    Fix issue with Path backslash

---
+++ read_structure_step/formats/mop/find_mopac.py
@@ -0,0 +1,52 @@
+import seamm
+import seamm_util
+from pathlib import Path
+import os
+
+mopac_error_identifiers = []
+
+
+def run_mopac(file_name):
+
+    try:
+        mopac_exe = '/opt/mopac/MOPAC2016.exe'
+
+        if os.path.isfile(mopac_exe) is False:
+            raise FileNotFoundError(
+                'The directory "/opt/mopac/" exists, but the executable \
+                "MOPAC2016.exe" is not there'
+            )
+
+    except FileNotFoundError:
+        try:
+
+            mopac_path = os.path.split(os.environ['mopac'])[0]
+            mopac_exe = mopac_path + 'MOPAC2016.exe'
+
+            if os.path.isfile(mopac_exe) is False:
+                raise FileNotFoundError(
+                    'The environment variable "mopac" is defined, but \
+                            the executable "MOPAC2016.exe" is not there'
+                )
+
+        except (KeyError, FileNotFoundError):
+            try:
+                mopac_exe = Path(os.environ['MOPAC_LICENSE']) / 'MOPAC2016.exe'
+                mopac_exe = str(mopac_exe)
+
+                if os.path.isfile(mopac_exe) is False:
+                    raise FileNotFoundError(
+                        'The environment variable "mopac" is defined, but the \
+                                executable "MOPAC2016.exe" is not there'
+                    )
+
+            except (KeyError, FileNotFoundError):
+                mopac_exe = seamm_util.check_executable("MOPAC2016.exe")
+
+                if mopac_exe is False:
+                    raise FileNotFoundError(
+                        'The MOPAC executable could not be found'
+                    )
+    local = seamm.ExecLocal()
+
+    local.run(cmd=[mopac_exe, file_name])
