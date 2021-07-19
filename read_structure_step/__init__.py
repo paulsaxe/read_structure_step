3c70d0c9d2849866c24fbfad39adb4d0a5c99644
Paul Saxe <paul@thesaxes.net>
2021-07-19 09:37:40 -0400

    Added writing functionality.

--- read_structure_step/__init__.py
+++ read_structure_step/__init__.py
@@ -12,9 +12,17 @@ from read_structure_step.read_structure import ReadStructure  # noqa: F401
 from read_structure_step.read_structure_parameters import (  # noqa: F401
     ReadStructureParameters,
 )
-from read_structure_step.read_structure_step import ReadStructureStep  # noqa: F401 E501
+from read_structure_step.read_structure_step import ReadStructureStep  # noqa: F401
 from read_structure_step.tk_read_structure import TkReadStructure  # noqa: F401
 from .read import read  # noqa: F401
+
+from read_structure_step.write_structure import WriteStructure  # noqa: F401
+from read_structure_step.write_structure_parameters import (  # noqa: F401
+    WriteStructureParameters,
+)
+from read_structure_step.write_structure_step import WriteStructureStep  # noqa: F401
+from read_structure_step.tk_write_structure import TkWriteStructure  # noqa: F401
+from .write import write  # noqa: F401
 
 # Handle versioneer
 from ._version import get_versions
