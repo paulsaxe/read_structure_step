b73d2a119e432600589470dc96a36109b69b4492
Paul Saxe <paul@thesaxes.net>
2021-07-19 09:38:12 -0400

    Added writing functionality.

--- read_structure_step/formats/__init__.py
+++ read_structure_step/formats/__init__.py
@@ -4,9 +4,12 @@ import sys
 import importlib
 
 from .registries import last_resort_reader
+from .registries import last_resort_writer
 from .openbabel_io.checkers import add_format_checkers
 from .openbabel_io.obabel import known_input_formats
 from .openbabel_io.obabel import load_file
+from .openbabel_io.obabel import known_output_formats
+from .openbabel_io.obabel import write_file
 
 path = os.path.join(os.path.dirname(__file__))
 modules = pkgutil.iter_modules(path=[path])
@@ -25,8 +28,9 @@ for loader, mod_name, ispkg in modules:
 
 del os, pkgutil, sys, importlib
 
-# Finally register the Open Babel reader as the last resort
+# Finally register the Open Babel reader and writer as the last resort
 last_resort_reader(known_input_formats, load_file)
+last_resort_writer(known_output_formats, write_file)
 
 # And the file format checkers
 add_format_checkers()
